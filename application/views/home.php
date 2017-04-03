8dbc10366ffffb389ca937f95bc7b9dd085f1348
martin-ukf <martin-ukf@users.noreply.github.com>
2017-04-03 22:44:46 +0200

    pridany jumbotron

--- application/views/home.php
+++ application/views/home.php
@@ -22,8 +22,12 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1><?php echo "Ahoj";?></h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        <div class="jumbotron">
+            <h1><?php echo "Ahoj";?></h1>
+            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+        
       </div>
 
     </div><!-- /.container -->
