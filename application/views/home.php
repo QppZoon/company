46474b5c2de8b4762594bbfa109fcc609f243813
martin-ukf <martin-ukf@users.noreply.github.com>
2017-04-03 22:26:24 +0200

    zmena configu

--- application/views/home.php
+++ application/views/home.php
@@ -22,7 +22,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1><?php echo "Ahoj";?></h1>
+        <h1><?php echo "Ahoj"; echo base_url();?></h1>
         <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
       </div>
 
