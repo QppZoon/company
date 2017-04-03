c93d1ff981855431a9212bf0911e54933d3aad8d
martin-ukf <martin-ukf@users.noreply.github.com>
2017-04-03 23:15:42 +0200

    prenos cez data

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,8 +20,9 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
+		$data['meno'] = "Karol";
 		$this->load->view('template/header');
-        $this->load->view('home');
+        $this->load->view('home',$data);
         $this->load->view('template/footer');
 	}
 }
--- application/views/home.php
+++ application/views/home.php
@@ -23,7 +23,7 @@
 
       <div class="starter-template">
         <div class="jumbotron">
-            <h1><?php echo "Ahoj";?></h1>
+            <h1><?php echo "Ahoj " . $meno;?></h1>
             <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
             <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
         </div>
